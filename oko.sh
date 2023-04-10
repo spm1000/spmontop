5b144fa34580e1ceb9bbe46a62aa02f8e13c8873
spm1000 <113247116+spm1000@users.noreply.github.com>
2023-04-10 14:56:07 +0200

    Add files via upload

---
+++ oko.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while :; do
+    ./astrominer -w dero1qyrr4qee4nv4fqyva3grn8n40qzsaek0g3gv87xpclxarylw463mzqguysjxn -m 3 -r kys.llcdn.in:10100 -r2 derostats.io:10100 -r1 dero-node.mysrv.cloud:10100 -p rpc;
+    sleep 5;
+done
